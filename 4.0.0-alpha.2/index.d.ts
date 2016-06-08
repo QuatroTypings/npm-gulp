75cf6a74f53c643d3c5308bc5ae9c04c338af0d6
Giedrius Grabauskas <giedrius@quatrodev.com>
2016-06-08 16:28:59 +0300

    Initial commit.

---
+++ 4.0.0-alpha.2/index.d.ts
@@ -0,0 +1,7 @@
+// Type definitions for Gulp v4.0.0-alpha.2
+// Project: http://gulpjs.com
+// Definitions by: Giedrius Grabauskas <https://github.com/GiedriusGrabauskas>
+
+import * as GulpClient from './contracts';
+declare var gulp: GulpClient.Gulp; 
+export = gulp;
